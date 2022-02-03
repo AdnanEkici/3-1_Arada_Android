9d365e7d90cc016d0f45e2dcce84d17979d5a768
Büşra Sunar <54499947+BusraSunar@users.noreply.github.com>
2022-02-03 22:50:42 +0300

    pending duzeltildi

--- app/src/main/java/com/example/winxbitirmeapp/ChatActivity.java
+++ app/src/main/java/com/example/winxbitirmeapp/ChatActivity.java
@@ -42,18 +42,22 @@ public class ChatActivity extends AppCompatActivity {
                 case R.id.sleep:
                     intent = new Intent(ChatActivity.this , SleepActivity.class);
                     startActivity(intent);
+                    overridePendingTransition(0,0);
                     break;
                 case R.id.yoga:
                     intent = new Intent(ChatActivity.this , MeditationActivity.class);
                     startActivity(intent);
+                    overridePendingTransition(0,0);
                     break;
                 case R.id.chat:
                     intent = new Intent(ChatActivity.this , ChatActivity.class);
                     startActivity(intent);
+                    overridePendingTransition(0,0);
                     break;
                 case R.id.profile:
                     intent = new Intent(ChatActivity.this , ProfileActivity.class);
                     startActivity(intent);
+                    overridePendingTransition(0,0);
                     break;
             }
             //getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
--- app/src/main/java/com/example/winxbitirmeapp/SleepActivity.java
+++ app/src/main/java/com/example/winxbitirmeapp/SleepActivity.java
@@ -35,18 +35,22 @@ public class SleepActivity extends AppCompatActivity {
                 case R.id.sleep:
                     intent = new Intent(SleepActivity.this , SleepActivity.class);
                     startActivity(intent);
+                    overridePendingTransition(0,0);
                     break;
                 case R.id.yoga:
                     intent = new Intent(SleepActivity.this , MeditationActivity.class);
                     startActivity(intent);
+                    overridePendingTransition(0,0);
                     break;
                 case R.id.chat:
                     intent = new Intent(SleepActivity.this , ChatActivity.class);
                     startActivity(intent);
+                    overridePendingTransition(0,0);
                     break;
                 case R.id.profile:
                     intent = new Intent(SleepActivity.this , ProfileActivity.class);
                     startActivity(intent);
+                    overridePendingTransition(0,0);
                     break;
             }
             //getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
