162de6cd104c84f41ea187d671b78b1e2af23e52
Büşra Sunar <54499947+BusraSunar@users.noreply.github.com>
2022-04-05 02:34:29 +0300

    splash ekrani eklendi

--- app/src/main/java/com/example/winxbitirmeapp/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/winxbitirmeapp/ForgotPasswordActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        email = (EditText) findViewById(R.id.forgotPassEmailEditText);
         setContentView(R.layout.activity_forgot_password);
     }
 
     public void sendEmailAddress(View view) {
+
     }
 }
